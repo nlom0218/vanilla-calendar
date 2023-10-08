f16bc60f2e4a54a14eb8eebf8510a2d88991a92a
nlom0218 <nlom0218@naver.com>
2023-10-08 14:30:50 +0900

    feat: 라이브러리 없이 자바스크립트로 달력 만들기 1 - Date() 생성자를 사용하여 Calendar 객체 만들기
    Calendar 객체에 getCalendarStorage 메서드 호출

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ const today = new Date();
 const year = today.getFullYear();
 const month = today.getMonth() + 1;
 
-const month1 = new Calendar(year, month); // 오늘 기준 23년 10월 달력
-const month2 = new Calendar(2023, 7); // 23년 7월 달력
-const month3 = new Calendar(2023, 8); // 23년 8월 달력
+const month1 = new Calendar(year, month);
+month1.getCalendarStorage(); // 오늘 기준 23년 10월 달력 정보
+
+const month2 = new Calendar(2023, 7);
+month2.getCalendarStorage(); // 23년 7월 달력 정보
+
+const month3 = new Calendar(2023, 8);
+month3.getCalendarStorage(); // 23년 8월 달력 정보
