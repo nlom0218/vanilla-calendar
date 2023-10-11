5a682cac91d5ab97a87bfc15d122811cf43cc0db
nlom0218 <nlom0218@naver.com>
2023-10-11 22:02:36 +0900

    feat: 라이브러리 없이 자바스크립트로 달력 만들기 2 - Schedule, Date Picker 만들기
    3. main.ts - Schedule Calendar 렌더링

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import Calendar from './Calendar';
+import Schedule from './ts/Schedule';
 
-const today = new Date();
-const year = today.getFullYear();
-const month = today.getMonth() + 1;
+class App {
+  #schedule;
 
-const month1 = new Calendar(year, month);
-month1.getCalendarStorage(); // 오늘 기준 23년 10월 달력 정보
+  constructor() {
+    this.#schedule = new Schedule();
+  }
 
-const month2 = new Calendar(2023, 7);
-month2.getCalendarStorage(); // 23년 7월 달력 정보
+  init() {
+    this.#schedule.render();
+  }
+}
 
-const month3 = new Calendar(2023, 8);
-month3.getCalendarStorage(); // 23년 8월 달력 정보
+const app = new App();
+app.init();
