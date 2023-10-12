ea297b272baa7cc020a7e977703461bb2d9bd215
nlom0218 <nlom0218@naver.com>
2023-10-12 16:22:39 +0900

    feat: 라이브러리 없이 자바스크립트로 달력 만들기 2 - Schedule, Date Picker 만들기
    6. main.ts - Date Picker Calendar 렌더링

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,39 @@
+import DatePicker from './ts/DatePicker';
 import Schedule from './ts/Schedule';
+import { $ } from './ts/dom';
 
 class App {
-  #schedule;
+  schedule;
+  datePicker;
 
   constructor() {
-    this.#schedule = new Schedule();
+    this.schedule = new Schedule();
+    this.datePicker = new DatePicker();
   }
 
   init() {
-    this.#schedule.render();
+    this.schedule.render();
+    this.registerEventListener();
   }
+
+  renderScheduleCalendar = () => this.schedule.render();
+
+  renderDatePickerCalendar = () => {
+    this.datePicker.initDatePicker();
+    this.datePicker.render();
+  };
+
+  registerEventListener = () => {
+    $('.schedule-button').addEventListener(
+      'click',
+      this.renderScheduleCalendar
+    );
+
+    $('.date-picker-button').addEventListener(
+      'click',
+      this.renderDatePickerCalendar
+    );
+  };
 }
 
 const app = new App();
