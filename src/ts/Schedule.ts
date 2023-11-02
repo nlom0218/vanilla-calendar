e92a7073082360bd409f909da10759b42e4cc8f3
nlom0218 <nlom0218@naver.com>
2023-11-02 22:48:08 +0900

    feat: 라이브러리 없이 자바스크립트로 달력 만들기 4 - Schedule에서 Navigation하기

--- src/ts/Schedule.ts
+++ src/ts/Schedule.ts
@@ -5,6 +5,7 @@ import yearMonthTemplate from './template/yearMonthTemplate';
 
 class Schedule {
   #calendar;
+  #isOpenNavigation = false;
 
   constructor() {
     const today = new Date();
@@ -26,6 +27,10 @@ class Schedule {
   };
 
   private renderCalendar = () => {
+    if (this.#isOpenNavigation) {
+      this.#isOpenNavigation = false;
+    }
+
     const getDayOfWeekClassName = (dayOfWeek: number) => {
       if (dayOfWeek === 0) return 'sunday';
 
@@ -70,9 +75,105 @@ class Schedule {
     this.registerEventListener();
   };
 
+  private renderNavigation = () => {
+    const navigationYear = this.#calendar.getNavigationYear();
+    const year = this.#calendar.getYear();
+
+    const isScheduleMonth = (month: number) => {
+      if (navigationYear !== year) return false;
+
+      return this.#calendar.getNavigationMonth() === month;
+    };
+
+    const navigationTemplate = `
+      <div class="schedule-navigation">
+        <div class="schedule-navigation-year">
+          <span>${navigationYear}</span>
+          <div class="schedule-navigation-year-buttons">
+            <button class="schedule-navigation-year-button" data-type="prev">◀︎</button>
+            <button class="schedule-navigation-year-button" data-type="next">▶︎</button>
+          </div>
+        </div>
+        <ul class="schedule-navigation-month-list">
+          ${Array.from({ length: 12 })
+            .map(
+              (_, index) =>
+                `<li class="schedule-navigation-month-item ${
+                  isScheduleMonth(index + 1) ? 'schedule-month' : ''
+                }" data-month="${index + 1}">${index + 1}월</li>`
+            )
+            .join('')}
+        </ul>
+      </div>
+    `;
+
+    $('.schedule-year-month').insertAdjacentHTML(
+      'beforeend',
+      navigationTemplate
+    );
+
+    this.registerNavigationEventListener();
+  };
+
+  private handleOpenNavigation = () => {
+    if (this.#isOpenNavigation) {
+      this.#isOpenNavigation = false;
+      $('.schedule-navigation').remove();
+
+      return;
+    }
+
+    this.#isOpenNavigation = true;
+    this.renderNavigation();
+  };
+
+  private handleNavigateYear: EventListener = (event) => {
+    const button = event.target;
+
+    if (!(button instanceof HTMLButtonElement)) return;
+
+    const type = button.dataset.type as 'next' | 'prev';
+    const newNavigationYear =
+      this.#calendar.getNavigationYear() + (type === 'next' ? +1 : -1);
+
+    this.#calendar.navigateYear(newNavigationYear);
+
+    $('.schedule-navigation').remove();
+    this.renderNavigation();
+  };
+
+  private handleNavigateCalendar: EventListener = (event) => {
+    const monthElement = event.target;
+
+    if (!(monthElement instanceof HTMLLIElement)) return;
+
+    const month = monthElement.dataset.month as string;
+
+    this.#calendar.navigateMonth(Number(month));
+    this.#calendar.navigate();
+
+    $('.schedule-navigation').remove();
+    this.renderCalendar();
+  };
+
   private registerEventListener = () => {
     $('.shift-month-button-container').addEventListener('click', (event) =>
       handleShiftMonth(event, this.#calendar, this.renderCalendar)
+    );
+
+    $('.schedule-navigation-button').addEventListener(
+      'click',
+      this.handleOpenNavigation
+    );
+  };
+
+  private registerNavigationEventListener = () => {
+    $('.schedule-navigation-year-buttons').addEventListener('click', (event) =>
+      this.handleNavigateYear(event)
+    );
+
+    $('.schedule-navigation-month-list').addEventListener('click', (event) =>
+      this.handleNavigateCalendar(event)
     );
   };
 }
--- src/ts/template/yearMonthTemplate.ts
+++ src/ts/template/yearMonthTemplate.ts
@@ -6,7 +6,13 @@ const yearMonthTemplate = (
     <div class=${
       calendar === 'schedule' ? 'schedule-year-month' : 'date-picker-year-month'
     }>
-        <span>${year}년 ${month}월</span>
+        <div>
+          <span>${year}년 ${month}월</span>
+          ${
+            calendar === 'schedule' &&
+            `<button class="schedule-navigation-button">▼</button>`
+          }
+        </div>
         <div class="shift-month-button-container">
             <button class="shift-month-button shift-mont-today-button" data-type="today">TODAY</button>
             <button class="shift-month-button shift-mont-arrow-button" data-type="prev">◀︎</button>
